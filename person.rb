70b8b3653cba4654f6256b16efaa6cb75d251a6e
Rafael Guzman <rega1237@gmail.com>
2022-07-18 22:11:26 -0400

    Add Person class

---
+++ person.rb
@@ -0,0 +1,25 @@
+class Person
+  def initialize(id, age, name = 'Unknown', parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def can_use_services?
+    if is_of_age? || @parent_permission
+      true
+    else
+      false
+    end
+  end
+
+  def of_age?
+    @age >= 18
+  end
+
+  private :is_of_age?
+end
