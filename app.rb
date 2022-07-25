914990daf2c81c568ecd3e40c1435a65d20198b1
Rafael Guzman <rega1237@gmail.com>
2022-07-25 16:17:59 -0400

    Make main as a class and add the method inputs to select

--- app.rb
+++ app.rb
@@ -13,19 +13,6 @@ class App
     @rentals = []
   end
 
-  # Show list of option to the user
-  def run
-    puts 'Please choose an option by enterin a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 = Exit'
-    user_select
-  end
-
   # Method for run by selection of user
   def user_select # rubocop:disable Metrics/CyclomaticComplexity
     selection = gets.chomp
--- main.rb
+++ main.rb
@@ -1,8 +1,24 @@
 require './app'
 
-def main
-  app = App.new
-  app.run
+class Main
+  attr_reader :app
+
+  def initialize
+    @app = App.new
+  end
+
+  def run
+    puts 'Please choose an option by enterin a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 = Exit'
+    user_select
+  end
 end
 
-main
+main = Main.new()
+main.run
