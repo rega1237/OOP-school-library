1bad6a8fe1cc2b87df8e2a06f85791aede3ae1d5
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-07-27 22:51:46 +0200

    Add test for book

---
+++ spec/book_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../book.rb'
+
+describe Book do 
+  context "When testing the Book class" do 
+     
+     it "The initialized correctly" do 
+        book = Book.new('Book Title', 'Book author')
+        expect(book.title).to eq "Book Title" 
+        expect(book.author).to eq "Book author"
+     end
+
+     it 'should has an array for rentals' do
+      book = Book.new('Book Title', 'Book author')
+      expect(book.rentals).to eq []
+    end
+     
+  end 
+end
