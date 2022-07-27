82b84df32827ea3d2379415034cfffa2f9b6fbea
Rafael Guzman <rega1237@gmail.com>
2022-07-27 18:48:51 -0400

    Create test for trimmer decorator and fix linters errors

--- spec/nameable_spec.rb
+++ spec/nameable_spec.rb
@@ -3,10 +3,9 @@ require_relative '../nameable'
 
 describe Nameable do
   context 'When testing the Nameable class' do
-
-    it "person should implement Nameable" do
+    it 'person should implement Nameable' do
       person = Person.new(16, 'Mark', parent_permission: false)
-      expect(person.correct_name).to eq "Mark"
+      expect(person.correct_name).to eq 'Mark'
     end
   end
 end
--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -6,7 +6,7 @@ describe Rental do
     it 'The initialized correctly' do
       student = Student.new('c12', 19, 'Mark', parent_permission: true)
       book = Book.new('Book Title', 'Book author')
-      rental = Rental.new("27/07/2022", student, book)
+      rental = Rental.new('27/07/2022', student, book)
       expect(rental.date).to eq '27/07/2022'
       expect(rental.book).to be_instance_of(Book)
       expect(rental.person).to be_instance_of(Student)
@@ -15,15 +15,15 @@ describe Rental do
     it 'rental should be added to a person' do
       student = Student.new('c12', 19, 'Mark', parent_permission: true)
       book = Book.new('Book Title', 'Book author')
-      rental = Rental.new("27/07/2022", student, book)
+      rental = Rental.new('27/07/2022', student, book)
       expect(rental.person.rentals.length).to eq 1
     end
 
     it 'rental should be added to a book' do
       student = Student.new('c12', 19, 'Mark', parent_permission: true)
       book = Book.new('Book Title', 'Book author')
-      rental = Rental.new("27/07/2022", student, book)
+      rental = Rental.new('27/07/2022', student, book)
       expect(rental.book.rentals.length).to eq 1
-    end  
+    end
   end
 end
