fc4374f301f38e3a7cce7591124d86e5d9da6115
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-07-28 00:40:58 +0200

    Add test to CapitalizeDecorator

---
+++ spec/nameable_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../person'
+require_relative '../nameable'
+
+describe Nameable do
+  context 'When testing the Nameable class' do
+
+    it "person should implement Nameable" do
+      person = Person.new(16, 'Mark', parent_permission: false)
+      expect(person.correct_name).to eq "Mark"
+    end
+  end
+end
